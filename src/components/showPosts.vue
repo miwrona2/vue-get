91d456df2653661b8e1674b85fc7552c26fd304d
Michal Wrona <michal.wrona@fitqbe.com>
2018-12-21 14:51:37 +0100

    simplest $http.get

---
+++ src/components/showPosts.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="show-posts">
+        <h1>All Articles</h1>
+        <div v-for="post in posts" class="single-post">
+            <h2>{{post.title}}</h2>
+            <article>{{ post.body }}</article>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            posts: {}
+        }
+    },
+    methods: {
+
+    },
+    created: function() {
+        this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(response){
+            this.posts = response.data.slice(0,10);
+        });
+    }
+}
+</script>
+
+<style scoped>
+#show-post {
+  max-width: 800px;
+  margin: 0 auto;  
+}
+.single-post {
+    padding: 20px;
+    margin: 20px 0;
+    box-sizing: border-box;
+    background: #eeeeee;
+}
+</style>
+
+
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import VueResource from 'vue-resource'
+import App from './App'
+
+Vue.use(VueResource)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
